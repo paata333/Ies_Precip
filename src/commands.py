bd83e25ebacd3189d3b68ee1713d08ffb20d95c0
paata333 <paata.shvelidze106@gmail.com>
2026-04-24 15:38:18 +0400

    Feat: added comment for init_db command

--- src/commands.py
+++ src/commands.py
@@ -16,6 +16,8 @@ from src import Config
 #     db.drop_all()
 #     db.create_all()
 #     click.echo("Database Created")
+
+#    warns user if he/she wants to delete data base 
 @click.command("init_db")
 @with_appcontext
 def init_db():
